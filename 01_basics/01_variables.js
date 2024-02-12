65b27f522e17da3d8e12de2c8659f375cbe31f80
Animesh71370 <156579101+Animesh71370@users.noreply.github.com>
2024-02-12 11:20:22 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 14451
+var accountPassword = 2304
+// accountEmail = 4
+account = "MUMBAI"
+console.log(account)
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,4 @@
+// alert(3+3)// we are using node js not browser
+
+console.log(3+3)
+let usenrame = true
