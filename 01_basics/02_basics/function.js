395115c57a812d9c7756c66f5e74348ead795ee2
Animesh71370 <156579101+Animesh71370@users.noreply.github.com>
2024-02-22 23:10:53 +0000

    function

---
+++ 01_basics/02_basics/function.js
@@ -0,0 +1,18 @@
+// function animesh(){
+
+// }
+// // animesh()
+// function addnum(num1,num2){
+//     return num1+num2;
+
+// }
+// console.log(addnum(3,4))
+function name (address){
+    console.log(`username is ${} and age is ${}`);
+}
+const user = {
+    name : "Animesh",
+    age: 21,
+    add : "Mumbai"
+
+}
--- 01_basics/05_objects.js
+++ 01_basics/05_objects.js
@@ -15,12 +15,39 @@ const jsUser = {
 
 
 
-jsUser.greeting =function(){
-    console.log("sup boiii");
-}
-console.log(jsUser.greeting());
+// jsUser.greeting =function(){
+//     console.log("sup boiii");
+// }
+// console.log(jsUser.greeting());
 //************************************************************************************************************** */
 
 //********************************  CONSTRUCTOR******************************************************/
+// const mobile =  new Object();
 
 
+
+// mobile.id = '123'
+// console.log(mobile);
+
+const identifycard = {
+    name: "Animesh",
+    age : 21,
+    address :{
+        streetadd: "Manvel Pada",
+        House_no: 204 
+    }
+};
+// console.log(identifycard);
+const adding = { ...jsUser , ...identifycard};
+console.log(adding.jsUser.age);
+
+//***************************************OBJECT ASSIGN IS USED FOR TO PRODUCE TARGET OBJECT FROM TWO DIFFERENT OBJECTS ALSO CAN BE USED FOR COMBINING OF TWO OBJECTS */
+
+
+
+
+const course = {
+    coursename: javascript,
+    price : 999,
+    instructir : "Animesh"
+}
