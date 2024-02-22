3b3b1558604990b3c179ab3b74d02661d717d9dd
Animesh71370 <156579101+Animesh71370@users.noreply.github.com>
2024-02-22 18:17:22 +0000

    05_objects

--- 01_basics/04_arrays.js
+++ 01_basics/04_arrays.js
@@ -4,5 +4,29 @@ const myarr = [0,1,2,3]
 
 //arrays methods
 myarr.push(6);
-console.log(myarr);
+
 myarr.pop()
+
+//SPLICE////SLICE*********************************************************************************
+// console.log("A ", myarr);
+const myn1 = myarr.slice(1,3)
+
+
+//in SPLICE it manipulate original array And in SLICE it manipulate copy of array
+
+
+
+//SPREAD METHOD **************************************************************************************
+const myfav = [1,2,3,4,5];
+const myfav1 = [6,7,8,9];
+const combinearr = [...myfav ,... myfav1];
+// console.log(combinearr);
+
+
+
+//***************FOR CONVERSION OF THE DATA WE USE isARRAY method */**************************************
+
+console.log(Array.isArray('Animesh'));
+console.log(Array.from('Animesh'));
+
+//**************************/
---
+++ 01_basics/05_objects.js
@@ -0,0 +1,26 @@
+
+
+////////********************************OBJECT LITERALS */
+const jsUser = {
+    name: "Animesh",
+    "salary":100000,
+    age : 22,
+    loc: "mumbai"
+}
+// console.log(jsUser.name);
+// console.log(jsUser["age"]);
+// console.log(jsUser["salary"]);
+// jsUser.name = "Animesh Singh";
+// console.log(jsUser.name);
+
+
+
+jsUser.greeting =function(){
+    console.log("sup boiii");
+}
+console.log(jsUser.greeting());
+//************************************************************************************************************** */
+
+//********************************  CONSTRUCTOR******************************************************/
+
+
