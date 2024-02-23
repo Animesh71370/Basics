f7fe5fc72acc08ea715c3ba03849f0bbe1e59f29
Animesh71370 <156579101+Animesh71370@users.noreply.github.com>
2024-02-23 18:19:56 +0000

    commit

--- 01_basics/loop.js
+++ 01_basics/loop.js
@@ -19,19 +19,40 @@
 // country.forEach( (lol) => {
 //     console.log(lol);
 // }    )
-//**********************HIGH ORDER LOOPS********************************************************************* */
-const arr = [
-    {
-        name : "Animesh"
-    },
-    {
-        age : 21
-    },
-    {
-        gender : "male"
-    },
-]
-arr.forEach((lol) => {
-    console.log(lol.name);
-} )
+// //**********************HIGH ORDER LOOPS********************************************************************* */
+// const arr = [
+//     {
+//         name : "Animesh"
+//     },
+//     {
+//         age : 21
+//     },
+//     {
+//         gender : "male"
+//     },
+// ]
+// arr.forEach((lol) => {
+//     console.log(lol.name);
+// } )
+//********************************FILTER METHOD ********************************************* */
+// const nums = [ 10,20,10,300,40,32,21,22]
+// const lol = nums.filter( (ans) => {
+//     return ans > 4
+// })
+// console.log(lol);
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ]
+  const idk = books.filter( (bk) => {
+    return bk.genre == "History" && bk.publish >= 1995
+  })
+  console.log(idk);
 
