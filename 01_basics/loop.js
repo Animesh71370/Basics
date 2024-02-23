b9b3d184beb1a0022532f6c96e664c563b0ea200
Animesh71370 <156579101+Animesh71370@users.noreply.github.com>
2024-02-23 17:38:18 +0000

    commit

--- 01_basics/loop.js
+++ 01_basics/loop.js
@@ -10,8 +10,28 @@
 // for (let index = 0; index < animesh.length; index++) {
 //     console.log(`show me all info : ${animesh[0]}`);
     
+// // }
+// const arr = [1,2,3,4,5,6]
+// for (const num of arr) {
+//     console.log(num);
 // }
-const arr = [1,2,3,4,5,6]
-for (const num of arr) {
-    console.log(num);
-}
+// const country = ["ind","pak","nepal"]
+// country.forEach( (lol) => {
+//     console.log(lol);
+// }    )
+//**********************HIGH ORDER LOOPS********************************************************************* */
+const arr = [
+    {
+        name : "Animesh"
+    },
+    {
+        age : 21
+    },
+    {
+        gender : "male"
+    },
+]
+arr.forEach((lol) => {
+    console.log(lol.name);
+} )
+
