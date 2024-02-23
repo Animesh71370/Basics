aff8cc78aed6a29b6a86f3d810c87ae76f0a13b0
Animesh71370 <156579101+Animesh71370@users.noreply.github.com>
2024-02-23 17:20:47 +0000

    commit

---
+++ 01_basics/loop.js
@@ -0,0 +1,17 @@
+// for (let index = 0; index < 5; index++) {
+//     console.log(`the value of outer loop is ${index}`);
+//     for (let ind = 0; ind < 10; ind++) {
+//         console.log(`the value of inner loop is ${ind}`);
+        
+//     }
+    
+// // }
+// const animesh = ["Surname","age","devloper"]
+// for (let index = 0; index < animesh.length; index++) {
+//     console.log(`show me all info : ${animesh[0]}`);
+    
+// }
+const arr = [1,2,3,4,5,6]
+for (const num of arr) {
+    console.log(num);
+}
